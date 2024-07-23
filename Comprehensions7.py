406d70ccd66c0b86130bb25b648ac3bd9696a8af
ErenAydogan <erenaydogan@live.fr>
2024-07-23 22:57:37 +0300

    Updated

---
+++ Comprehensions7.py
@@ -0,0 +1,10 @@
+import os
+
+folder_path = os.path.dirname(__file__)
+file_path = os.path.join(folder_path, 'python.txt')
+
+op = open(file_path, 'r')
+
+output = [i for i in op if "comprehension" in i]
+
+print(output)
