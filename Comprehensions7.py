ac9f715e505450033c7f6a5da9397ca60c76b719
ErenAydogan <erenaydogan@live.fr>
2024-07-26 12:47:40 +0300

    Updated

--- Comprehensions7.py
+++ Comprehensions7.py
@@ -5,6 +5,6 @@ file_path = os.path.join(folder_path, 'python.txt')
 
 op = open(file_path, 'r')
 
-output = [i for i in op if "comprehension" in i]
+output = [i for i in op if "our" in i]
 
 print(output)
