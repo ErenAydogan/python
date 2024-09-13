2d1fe45371eb360602fba22147238a6df9266336
ErenAydogan <erenaydogan@live.fr>
2024-09-13 23:27:56 +0300

    Updated

---
+++ Descriptors21.py
@@ -0,0 +1,23 @@
+import time
+
+class Lazy:
+    def __init__(self, function):
+        self.function=function
+        self.name = function.__name__
+
+    def __get__(self, obj, type=None) -> object:
+        obj.__dict__[self.name]=self.function(obj)
+        return obj.__dict__[self.name]
+
+class Waiting:
+    @Lazy
+    def wait(self):
+        time.sleep(3)
+        return 42
+    
+
+x = Waiting()
+
+print(x.wait)
+print(x.wait)
+print(x.wait)
